22502e991fccea0d412bd6016da81cf7011b6039
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-02 21:15:17 +0400

    - Fixed public IP

--- reserved_ips.tf
+++ reserved_ips.tf
@@ -1,7 +1,7 @@
 # Create a reserved public IP for the nat gateway
 resource "oci_core_public_ip" "prod_nat" {
   compartment_id = data.doppler_secrets.prod_main.map.OCI_GAIA_COMPARTMENT_PRODUCTION_ID
-  lifetime       = "Reserved"
+  lifetime       = "RESERVED"
 
   display_name = "res-prod-nat"
 
@@ -11,7 +11,7 @@ resource "oci_core_public_ip" "prod_nat" {
 # Create a reserved public IP for the web network load balancer
 resource "oci_core_public_ip" "web_nlb" {
   compartment_id = data.doppler_secrets.prod_main.map.OCI_GAIA_COMPARTMENT_PRODUCTION_ID
-  lifetime       = "Reserved"
+  lifetime       = "RESERVED"
 
   display_name = "res-web-nlb"
 
