71d57399d5921482bd99fed4f72511813c3c7523
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-03 16:29:13 +0400

    - Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 
-# Output the public IP for web-01
-output "web-01-public-ip" {
-  value = oci_core_instance.web_01.public_ip
+# Output the public IP for web NSG reserved IP
+output "web_nsg_reserved_ip" {
+  value = oci_core_public_ip.web_nlb.ip_address
+}
+
+
+# Output the public IP for NAT gateway public ip
+output "nat_gateway_public_ip" {
+  value = oci_core_public_ip.prod_nat.ip_address
 }
 
 # Output the private IP for web-01
-output "web-01-private-ip" {
+output "web_01_private_ip" {
   value = oci_core_instance.web_01.private_ip
 }
 
-
-# Output the public IP for web-02
-output "web-02-public-ip" {
-  value = oci_core_instance.web_02.public_ip
-}
-
 # Output the private IP for web-02
-output "web-02-private-ip" {
+output "web_02_private_ip" {
   value = oci_core_instance.web_02.private_ip
 }
