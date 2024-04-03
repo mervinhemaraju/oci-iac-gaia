a0fb8cdcbb2528a295c580c0d0bc5e2623df7ffc
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-03 15:36:01 +0400

    - Added policy to manage load balancers

---
+++ policies.tf
@@ -0,0 +1,15 @@
+resource "oci_identity_policy" "administrators_load_balancers" {
+  compartment_id = data.doppler_secrets.prod_main.map.OCI_GAIA_COMPARTMENT_ROOT_ID
+  description    = "Allow Administrators to manage load balancers"
+  name           = "allow-administrators-manage-load-balancers"
+
+  statements = [
+    "Allow group Administrators to manage load-balancers in tenancy",
+    "Allow group Administrators to manage network-load-balancers in tenancy",
+    "Allow group Administrators to use load-balancers in tenancy",
+    "Allow group Administrators to use network-load-balancers in tenancy",
+    "Allow group Administrators to use virtual-network-family in tenancy",
+  ]
+
+  freeform_tags = local.tags.defaults
+}
