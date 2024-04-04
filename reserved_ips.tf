99e53dfaa95bcea0fe9b22f8ff53657620e91e32
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-04 14:26:05 +0400

    - Added lifecycle block to ignore private ip id in reserved public ip for nlb

--- reserved_ips.tf
+++ reserved_ips.tf
@@ -9,11 +9,17 @@ resource "oci_core_public_ip" "prod_nat" {
 }
 
 # Create a reserved public IP for the web network load balancer
-# resource "oci_core_public_ip" "web_nlb" {
-#   compartment_id = data.doppler_secrets.prod_main.map.OCI_GAIA_COMPARTMENT_PRODUCTION_ID
-#   lifetime       = "RESERVED"
+resource "oci_core_public_ip" "web_nlb" {
+  compartment_id = data.doppler_secrets.prod_main.map.OCI_GAIA_COMPARTMENT_PRODUCTION_ID
+  lifetime       = "RESERVED"
 
-#   display_name = "res-web-nlb"
+  display_name = "res-web-nlb"
 
-#   freeform_tags = local.tags.defaults
-# }
+  freeform_tags = local.tags.defaults
+
+  lifecycle {
+    ignore_changes = [
+      private_ip_id
+    ]
+  }
+}
