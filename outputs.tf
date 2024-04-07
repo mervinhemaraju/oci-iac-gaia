936ed13f3f11e3ba94aa2d47f673a7f7ed762765
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-04-07 12:43:20 +0400

    - Updated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
-# Output the public IP for web NSG reserved IP
-output "web_nsg_reserved_ip" {
+# Output the public IP for web NLB reserved IP
+output "web_nlb_reserved_ip" {
   value = oci_core_public_ip.web_nlb.ip_address
 }
 
@@ -14,7 +14,17 @@ output "web_01_private_ip" {
   value = oci_core_instance.web_01.private_ip
 }
 
+# Output the public IP for web-01
+output "web_01_private_ip" {
+  value = oci_core_instance.web_01.public_ip
+}
+
 # Output the private IP for web-02
 output "web_02_private_ip" {
   value = oci_core_instance.web_02.private_ip
+}
+
+# Output the public IP for web-02
+output "web_02_private_ip" {
+  value = oci_core_instance.web_02.public_ip
 }
