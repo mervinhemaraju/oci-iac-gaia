e34b6dfa16a74cbbcca0300dcbfef61b2efcbcfe
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-05-02 15:38:42 +0400

    - Added network details for DRG attachments

--- drg_attachment.tf
+++ drg_attachment.tf
@@ -8,14 +8,9 @@ resource "oci_core_drg_attachment" "database_cross_tenancy" {
 
   #   drg_route_table_id = oci_core_drg_route_table.test_drg_route_table.id
 
-  # network_details {
-  #     #Required
-  #     id = oci_core_vcn.test_vcn.id
-  #     type = var.drg_attachment_network_details_type
-
-  #     #Optional
-  #     id = var.drg_attachment_network_details_id
-  #     route_table_id = oci_core_route_table.test_route_table.id
-  #     vcn_route_type = var.drg_attachment_network_details_vcn_route_type
-  # }
+  network_details {
+    id             = oci_core_vcn.web.id
+    type           = "VCN"
+    vcn_route_type = "SUBNET_CIDRS"
+  }
 }
