f581c6baad74867cea739c67713c134f68ab2f49
Mervin Hemaraju <th3pl4gu33@gmail.com>
2024-05-02 19:57:16 +0400

    - Added a route to the database drg in the public subnet route table

--- route_tables.tf
+++ route_tables.tf
@@ -15,6 +15,15 @@ resource "oci_core_route_table" "public_web" {
     destination_type = "CIDR_BLOCK"
   }
 
+  route_rules {
+
+    network_entity_id = data.oci_core_drgs.database.drgs[0].id
+
+    description      = "Route to the database vcn (Helios VCN peering via DRG)"
+    destination      = "10.16.0.0/16"
+    destination_type = "CIDR_BLOCK"
+  }
+
   freeform_tags = local.tags.defaults
 }
 
