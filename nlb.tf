318be0627e7e3a8ee9e13ab17bc4ce66c6288e55
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-01-30 15:41:31 +0400

    - Removed nlb

--- nlb.tf
+++ nlb.tf
@@ -1,78 +1,78 @@
 
-# Create a network load balancer for web servers
-resource "oci_network_load_balancer_network_load_balancer" "web" {
+# # Create a network load balancer for web servers
+# resource "oci_network_load_balancer_network_load_balancer" "web" {
 
-  compartment_id                 = local.values.compartments.production
-  display_name                   = "web"
-  subnet_id                      = oci_core_subnet.public_web.id
-  freeform_tags                  = local.tags.defaults
-  is_preserve_source_destination = false
-  is_private                     = false
-  network_security_group_ids = [
-    oci_core_network_security_group.web.id
-  ]
+#   compartment_id                 = local.values.compartments.production
+#   display_name                   = "web"
+#   subnet_id                      = oci_core_subnet.public_web.id
+#   freeform_tags                  = local.tags.defaults
+#   is_preserve_source_destination = false
+#   is_private                     = false
+#   network_security_group_ids = [
+#     oci_core_network_security_group.web.id
+#   ]
 
-  reserved_ips {
-    id = oci_core_public_ip.web_nlb.id
-  }
-}
+#   reserved_ips {
+#     id = oci_core_public_ip.web_nlb.id
+#   }
+# }
 
-# NSG Backend Sets
-resource "oci_network_load_balancer_backend_set" "https" {
+# # NSG Backend Sets
+# resource "oci_network_load_balancer_backend_set" "https" {
 
-  name                     = "https-backend"
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
-  policy                   = "FIVE_TUPLE"
+#   name                     = "https-backend"
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
+#   policy                   = "FIVE_TUPLE"
 
 
-  health_checker {
+#   health_checker {
 
-    protocol = "HTTP"
+#     protocol = "HTTP"
 
-    interval_in_millis  = 10000
-    port                = 80
-    response_body_regex = "OK"
-    retries             = 3
-    return_code         = 200
-    timeout_in_millis   = 3000
-    url_path            = "/health"
-  }
+#     interval_in_millis  = 10000
+#     port                = 80
+#     response_body_regex = "OK"
+#     retries             = 3
+#     return_code         = 200
+#     timeout_in_millis   = 3000
+#     url_path            = "/health"
+#   }
 
-  is_preserve_source = false
-}
+#   is_preserve_source = false
+# }
 
-# NSG Backends
-resource "oci_network_load_balancer_backend" "web_01" {
-  backend_set_name         = oci_network_load_balancer_backend_set.https.name
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
-  port                     = 443
+# # NSG Backends
+# resource "oci_network_load_balancer_backend" "web_01" {
+#   backend_set_name         = oci_network_load_balancer_backend_set.https.name
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
+#   port                     = 443
 
-  is_backup  = false
-  is_drain   = false
-  is_offline = false
-  name       = oci_core_instance.web_01.display_name
-  target_id  = oci_core_instance.web_01.id
-  weight     = 1
-}
+#   is_backup  = false
+#   is_drain   = false
+#   is_offline = false
+#   name       = oci_core_instance.web_01.display_name
+#   target_id  = oci_core_instance.web_01.id
+#   weight     = 1
+# }
 
-resource "oci_network_load_balancer_backend" "web_02" {
-  backend_set_name         = oci_network_load_balancer_backend_set.https.name
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
-  port                     = 443
+# resource "oci_network_load_balancer_backend" "web_02" {
+#   backend_set_name         = oci_network_load_balancer_backend_set.https.name
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
+#   port                     = 443
 
-  is_backup  = false
-  is_drain   = false
-  is_offline = false
-  name       = oci_core_instance.web_02.display_name
-  target_id  = oci_core_instance.web_02.id
-  weight     = 1
-}
+#   is_backup  = false
+#   is_drain   = false
+#   is_offline = false
+#   name       = oci_core_instance.web_02.display_name
+#   target_id  = oci_core_instance.web_02.id
+#   weight     = 1
+# }
 
-# NSG Listeners
-resource "oci_network_load_balancer_listener" "https" {
-  default_backend_set_name = oci_network_load_balancer_backend_set.https.name
-  name                     = "https-listeners"
-  network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
-  port                     = 443
-  protocol                 = "TCP_AND_UDP"
-}
+# # NSG Listeners
+# resource "oci_network_load_balancer_listener" "https" {
+#   default_backend_set_name = oci_network_load_balancer_backend_set.https.name
+#   name                     = "https-listeners"
+#   network_load_balancer_id = oci_network_load_balancer_network_load_balancer.web.id
+#   port                     = 443
+#   protocol                 = "TCP_AND_UDP"
+# }
