342269c50483565d2ce61934a06ff30b29ad8771
mervin.hemaraju <mervinhemaraju16@gmail.com>
2025-03-05 01:51:03 +0400

    - Migration of state file

--- providers.tf
+++ providers.tf
@@ -26,8 +26,15 @@ provider "oci" {
 # * The Terraform Module
 terraform {
 
-  # * The required tf version
-  required_version = "1.4.0"
+  # The required tf version
+  required_version = "1.8.7"
+
+  # Backend configuration
+  backend "s3" {
+    region = var.bucket_region
+    key    = "${var.bucket_key_prefix_iac}/state.tf"
+    bucket = var.bucket_name
+  }
 
   # * Required providers
   required_providers {
--- variables.tf
+++ variables.tf
@@ -1,3 +1,7 @@
+
+/**
+* Mandatory variables
+**/
 variable "region" {
   type        = string
   description = "The region where the resources will be created."
@@ -8,3 +12,22 @@ variable "token_doppler_iac_cloud_main" {
   type        = string
   description = "The Doppler token for the secrets manager cloud main repo."
 }
+
+variable "bucket_key_prefix_iac" {
+  type        = string
+  description = "The prefix for the bucket key."
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the bucket."
+}
+
+variable "bucket_region" {
+  type        = string
+  description = "The region of the bucket."
+}
+
+/**
+* End of Mandatory variables
+**/
