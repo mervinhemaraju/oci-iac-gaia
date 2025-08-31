a122703f36800e1e77c0a20c7208dbac5e95fffa
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-31 13:47:59 +0400

    - Fixed issue in rpc connection

--- components/300-networking/rpc.tf
+++ components/300-networking/rpc.tf
@@ -4,8 +4,8 @@ resource "oci_core_remote_peering_connection" "database" {
   drg_id         = oci_core_drg.database.id
 
   display_name     = "database-rpc"
-  peer_id          = local.networking.gateways.rpc_id_poseidon
-  peer_region_name = var.region
+  peer_id          = local.networking.gateways.rpc_id_poseidon # Acceptor Poseidon rpc id
+  peer_region_name = "uk-south-1"                              # Acceptor Poseidon region
 
   freeform_tags = local.tags.defaults
 }
