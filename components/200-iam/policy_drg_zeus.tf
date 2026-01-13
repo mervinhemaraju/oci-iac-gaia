3b763b748985d46c57da8c96e276db0862d6a651
Mervin Hemaraju <th3pl4gu33@gmail.com>
2026-01-13 11:07:15 +0400

    - Fixed policies issues

--- components/200-iam/policy_drg_zeus.tf
+++ components/200-iam/policy_drg_zeus.tf
@@ -7,26 +7,46 @@ resource "oci_identity_policy" "zeus_cross_conection_statements" {
   description    = "Allow the requestor to manage DRG and RPC in this tenancy and account ZEUS tenancy."
 
   statements = [
+    # # Definitions
+    # "define group ${oci_identity_group.drg_admins.name} as ${oci_identity_group.drg_admins.id}",
+    # "define group ${local.values.groups.zeus_groups.vcn_admins.name} as ${local.values.groups.zeus_groups.vcn_admins.id}",
+    # "define tenancy zeusTenancy as ${local.values.compartments.root_zeus}",
+
+    # # Allow
+    # "Allow group ${oci_identity_group.drg_admins.name} to manage virtual-network-family in tenancy",
+    # "Allow group ${local.values.groups.zeus_groups.vcn_admins.name} to manage virtual-network-family in tenancy",
+    # "allow group ${oci_identity_group.drg_admins.name} to manage local-peering-gateways in tenancy",
+
+    # # Endorse
+    # "endorse group ${oci_identity_group.drg_admins.name} to manage drg-attachment in tenancy zeusTenancy",
+
+    # # Admit
+    # "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage drg in tenancy",
+    # "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage local-peering-from in tenancy",
+    # "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage virtual-network-family in tenancy",
+
+    # # Explicitly add associate permission for clarity
+    # "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to associate local-peering-gateways in tenancy",
+
     # Definitions
     "define group ${oci_identity_group.drg_admins.name} as ${oci_identity_group.drg_admins.id}",
     "define group ${local.values.groups.zeus_groups.vcn_admins.name} as ${local.values.groups.zeus_groups.vcn_admins.id}",
     "define tenancy zeusTenancy as ${local.values.compartments.root_zeus}",
 
-    # Allow
-    "Allow group ${oci_identity_group.drg_admins.name} to manage virtual-network-family in tenancy",
-    "Allow group ${local.values.groups.zeus_groups.vcn_admins.name} to manage virtual-network-family in tenancy",
+    # --- ALLOW (Local Gaia admins managing Gaia resources) ---
+    "allow group ${oci_identity_group.drg_admins.name} to manage virtual-network-family in tenancy",
     "allow group ${oci_identity_group.drg_admins.name} to manage local-peering-gateways in tenancy",
 
-    # Endorse
+    # --- ENDORSE (Local Gaia admins going to Zeus) ---
     "endorse group ${oci_identity_group.drg_admins.name} to manage drg-attachment in tenancy zeusTenancy",
 
-    # Admit
-    "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage drg in tenancy",
-    "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage local-peering-from in tenancy",
+    # --- ADMIT (Remote Zeus admins coming into Gaia) ---
+    # This is the critical block for the LPG connection to succeed
     "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage virtual-network-family in tenancy",
-
-    # Explicitly add associate permission for clarity
+    "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage local-peering-from in tenancy",
     "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to associate local-peering-gateways in tenancy",
+    "admit group ${local.values.groups.zeus_groups.vcn_admins.name} of tenancy zeusTenancy to manage drg in tenancy"
+
   ]
 
   freeform_tags = local.tags.defaults
